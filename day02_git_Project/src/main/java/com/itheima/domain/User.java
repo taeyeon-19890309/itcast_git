cc08579aee93b8e4dbf45b626a151fd4785998a3
Taeyeon <taeyeon_19890309@126.com>
2020-02-08 20:18:30 +0800

    新建User.java提交到本地仓库

---
+++ day02_git_Project/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * @author: Taeyeon
+ * @date: 2020/2/8 20:10
+ * @Version V1.0
+ */
+public class User {
+
+    private Integer id;
+    private String name;
+}
